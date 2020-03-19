5745d5172583713d0e22b5416929d0d75c68f6bb
Andy Bell <me@andy-bell.co.uk>
2020-03-19 13:32:34 +0000

    Limit and shuffle the reels

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,29 @@ module.exports = config => {
     return collection.getAll().find(x => x.url === '/');
   });
 
+  // Limit feed items and shuffle them so they are random for
+  // each build
+  config.addFilter('shuffle', items => {
+    let currentIndex = items.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = items[currentIndex];
+      items[currentIndex] = items[randomIndex];
+      items[randomIndex] = temporaryValue;
+    }
+
+    // Limit response to 1- items
+    return items.slice(0, 10);
+  });
+
   // Pass through
   config.addPassthroughCopy('./src/images/');
   config.addPassthroughCopy('./src/site.webmanifest');
